948b94b454067acc04e86e15c872d2832717e407
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-07-11 18:08:12 -0300

    create property list component

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container flex flex-col h-screen"> 
+    <div class="py-4">
+      <h1 class="text-2xl">Accordous</h1> 
+      <properties-list :properties="properties"></properties-list>     
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import propertyService from './services/property.service'
+import PropertiesList from './components/PropertiesList'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      properties: []
+    } 
+  },
+  created() {
+    propertyService.list().then(response => {
+        this.properties = response.data.data
+      })
+      .catch(error => {
+          console.log(error)
+      })
+  },
   components: {
-    HelloWorld
-  }
+    'properties-list': PropertiesList
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ app/src/components/PropertiesList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <table v-if="properties.length>0" class="table striped mb-4 text-md">
+            <thead>
+                <tr>
+                    <th>Email</th>
+                    <th>Endereço</th>
+                    <th>Status</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="property in properties" :key="property.id">
+                    <td>{{property.email}}</td>
+                    <td>{{property.street}}, {{property.number}}, {{property.city.name}}, {{property.state.name}}</td>
+                    <td width="10%">
+                        <span class="badge badge-success">contratado</span>
+                    </td>
+                    <td width="5%">
+                        <a>Remover</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div v-else>
+            <div class="text-center">Sem propriedades cadastradas</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        properties: {
+            //required: true,
+            type: Array
+        }
+    }    
+}
+</script>
