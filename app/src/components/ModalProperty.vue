765681d0aa1e7ca1cbe190a3efbc7cd92df2da28
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-07-11 23:00:08 -0300

    [app] create property form

--- app/src/components/ModalProperty.vue
+++ app/src/components/ModalProperty.vue
@@ -2,7 +2,67 @@
     <modal :showing="showing" @close="close">
         <div class="modal-title">Cadastrar propriedade</div>
         <div class="modal-body">
-            Formulário
+            <form class="px-2 pt-6 pb-2 mb-4">
+                <div class="mb-4">
+                    <label for="" class="label">
+                        Email do proprietário
+                     </label>
+                    <input type="email" class="form-control" placeholder="email@dominio.com" ref="email"/>
+                </div>
+                
+                <div class="flex flex-wrap -mx-3 mb-2">
+                    <div class="w-full md:w-3/4 px-3">
+                         <label class="label" for="grid-password">
+                            Rua
+                        </label>
+                        <input class="form-control" type="text" placeholder="Rua, Avenida, Travessa, etc.">
+                     </div>
+                    <div class="w-full md:w-1/4 px-3">
+                        <label class="label" for="grid-password">
+                            Número
+                        </label>
+                        <input class="form-control" type="text">
+                    </div>
+                </div>
+
+                <div class="flex flex-wrap -mx-3 mb-2">
+                    <div class="w-full md:w-3/5 px-3">
+                        <label class="label" for="grid-password">
+                            Complemento
+                        </label>
+                        <input class="form-control" type="text">
+                    </div>
+
+                    <div class="w-full md:w-2/5 px-3">
+                        <label class="label" for="grid-password">
+                            Bairro
+                        </label>
+                        <input class="form-control" type="text">
+                    </div>
+                </div>
+
+                <div class="flex flex-wrap -mx-3 mb-2">
+                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label for="" class="label">Estado</label>
+                        <select-state 
+                            empty="Selecione um estado"
+                            :options="states"
+                            :loading="loadingStates"
+                            v-model="state"
+                            @input="loadCities" 
+                        />
+                    </div>
+                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label for="" class="label">Cidade</label>
+                        <select-city 
+                            empty="Selecione uma cidade"
+                            :options="cities"
+                            :loading="loadingCities"
+                            v-model="city"
+                        />
+                    </div>
+                </div>
+            </form>
         </div>
         <div class="modal-footer">
             <a @click.prevent.stop="save" href="#" class="btn btn-primary">Salvar</a>
@@ -12,9 +72,21 @@
 </template>
 
 <script>
-import Modal from '@/shared/Modal';
+import Modal from '@/shared/Modal'
+import Select from '@/shared/Select'
+import stateCityService from '@/services/stateCity.service'
 
 export default {
+    data() {
+        return {            
+            states: [],
+            cities: [],
+            loadingStates: false,
+            loadingCities: false,
+            state: -1,
+            city: -1
+        }
+    },
     props: {
         showing: {
             required: true,
@@ -23,14 +95,47 @@ export default {
     },
     methods: {
         close () {
+            this.state = -1
+            this.city = -1
             this.$emit('close')
         },
         save () {
             this.$emit('close', {user:'sdfsdf'})
+        },
+        loadStates() {
+            this.loadingStates = true;
+            stateCityService.listStates()
+                .then((response) => {
+                    this.loadingStates = false;
+                    this.states = response.data.data.map(makeOptions)
+                })
+        },
+        loadCities() {
+           this.loadingCities = true;
+            stateCityService.listCities({state:this.state})
+                .then((response) => {
+                    this.loadingCities = false;
+                    this.cities = response.data.data.map(makeOptions)
+                })
         }
     },
     components: {
-        'modal': Modal
+        'modal': Modal,
+        'select-state': Select,
+        'select-city': Select,
+    },
+    watch: {
+        showing (value) {
+            if(value) {
+                this.$nextTick(() => {
+                    this.$refs["email"].focus();
+                });
+                this.loadStates();
+            }
+        }
     }
+}
+function makeOptions(item) {
+    return {text:item.name, value:item.id}
 }
 </script>
---
+++ app/src/shared/Select.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="relative">
+        <select v-model="model" :disabled="loading" class="appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+            <option :value="-1" v-if="loading" disabled>Carregando...</option>
+            <option :value="-1" v-else disabled>{{empty}}</option>
+            <option 
+                v-for="option in options" 
+                :value="option.value" 
+                :key="option.value"            
+            >
+                {{option.text}}
+            </option>
+        </select>
+        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        value:{
+            required: false
+        },
+        loading: {
+            type: Boolean
+        },
+        options: {
+            type: Array
+        },
+        empty: {
+            type: String
+        }
+    },
+    computed:{
+        model:{
+            get(){
+                return this.value
+            },
+            set(val){
+                this.$emit('input', val)
+            }
+        }
+    }, 
+}
+</script>
