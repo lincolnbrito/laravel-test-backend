8829b39a5ecf4c006cca072ff63754e0c5a26f42
Lincoln Brito <lincoln.sbrito@gmail.com>
2020-07-12 15:52:00 -0300

    [api] improve hirer_document validation

--- api/app/Http/Requests/ContractStoreRequest.php
+++ api/app/Http/Requests/ContractStoreRequest.php
@@ -24,9 +24,10 @@ class ContractStoreRequest extends FormRequest
      */
     public function rules()
     {
+        $validateRex = request('hirer_type') == 'F' ? "/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/" : "/^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/";
         return [
             'hirer_type' => 'required',
-            'hirer_document' => 'required|regex:/^([0-9]*)$/',
+            'hirer_document' => 'required|regex:'.$validateRex,
             'hirer_email' => 'required|email',
             'hirer_name' => 'required',
             'property_id' => 'required|unique:contracts|exists:properties,id'
